80ee6b029efe9986a7568759dfda539b4d366586
anas2002 <anas4112002@gmail.com>
2023-06-08 02:23:05 +0530

    final commit

--- app/src/main/java/com/anas/firebaseqrscanner/MainActivity.java
+++ app/src/main/java/com/anas/firebaseqrscanner/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.Result;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -24,19 +25,44 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     public static TextView txtScanned;
+    Button btnFirestore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         txtScanned=findViewById(R.id.txtScanned);
+        btnFirestore = findViewById(R.id.btnFirestore);
+
         findViewById(R.id.btnScan).setOnClickListener(v -> {
-
-
             startActivity(new Intent(getApplicationContext(),QRActivity.class));
         });
+
+        btnFirestore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String scannedText = txtScanned.getText().toString();
+
+
+                Map<String, Object> data = new HashMap<>();
+                data.put("text", scannedText);
+
+                FirebaseFirestore.getInstance().collection("Scanned Texts")
+                        .add(data)
+                        .addOnSuccessListener(documentReference -> {
+                            Toast.makeText(MainActivity.this, "Saved to Firestore", Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(MainActivity.this, "Failed to save", Toast.LENGTH_SHORT).show();
+                        });
+
+            }});
     }
 }
